ddfaa3ba418d594555001cfb6c57badba513f09c
jarturomora <jarturomora@gmail.com>
2015-09-24 07:32:36 -0500

    Code added in section2_4.r to plot a box plot with labels using the fivenum() function.

--- section2_4.r
+++ section2_4.r
@@ -15,7 +15,7 @@ print(fivenum(temps))
 # Contrasting a box plot Vs. a histogram
 
 # Setting the plot area
-par(mfrow = c(2,1))
+par(mfrow = c(3,1))
 # Creating the box plot
 boxplot(temps, main = "Box Plot of Summet High Temps", ylim = c(50, 110), xlab = "Temperature (deg F)", horizontal = T, col = "blue")
 # Creating the histogram
@@ -24,3 +24,18 @@ hist(temps,  main = "Histogram Plot of Summet High Temps", xlim = c(50, 110), xl
 abline(v = mean(temps), lwd = 5, col = "red")
 # Adding a vertical line  at the meadian
 abline(v = median(temps), lwd = 3, col = "blue")
+
+# Drawing a box plot with comprenhensive labeling
+
+# Generating random data to plot
+dfx <- data.frame(
+	group = c(rep('A', 8), rep('B', 15), rep('C',6)),
+	sex = sample(c("M", "F"), size = 29, replace = T),
+	age = runif(n = 29, min = 18, max = 54)
+)
+# Define labels for the plot
+my.labels <- round(fivenum(dfx$age), digits = 2)
+# Creating the box plot for the Age variable of the dfx data frame
+boxplot(dfx$age, horizontal = T, ylim = c(10, 60), col = "lightgray", add = F, main = "Distribution of Ages")
+# Finally we add the labels we define previously to put the five numbers over the box plot.
+text(x = my.labels, y = c(1.15, 1.25, 1.25, 1.25, 1.15), labels = my.labels)
