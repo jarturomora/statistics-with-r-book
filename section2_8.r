765c74f739f334ed17264427235b061e9b8dfa6e
jarturomora <jarturomora@gmail.com>
2015-09-30 09:08:00 -0500

    Code added to Section 2.8 script to plot examples of how different skewdness produces different shapes in the QQ Plot.

--- section2_8.r
+++ section2_8.r
@@ -37,10 +37,37 @@ if (is.normal(test.scores$score)) print ("The exam scores are normally distribut
 
 # Open a new window to plot normal data example
 new.window("bill")
+par(mfrow = c(2, 1))
 
 # Create ideal normal data
 normal <- rnorm(500, 46.85, 11.31)
-par(mfrow = c(2, 1))
-hist(normal, breaks = 12, xlim = c(0, 100), ylim = c(0, 100), main = "N(46.85, 11.31)", col = "white")
+hist(normal, breaks = 12, xlim = c(0, 100), ylim = c(0, 100), main = "Normally Distributed Data - N(46.85, 11.31)", col = "green")
 qqnorm(normal)
 qqline(normal)
+
+# Skewed to the righ data
+new.window("bill")
+par(mfrow = c(2, 1))
+label <- "Unimodal, Skewed Right"
+x <- rbeta(100000,2,5)
+hist(x,col="orange",breaks=100,axes=FALSE,xlab="",main=label)
+qqnorm(x)
+qqline(x)
+
+# Skewed to the left data
+new.window("bill")
+par(mfrow = c(2, 1))
+label <- "Unimodal, Skewed Left"
+x <- rbeta(100000,5,2)
+hist(x,col="magenta",breaks=100,axes=FALSE,xlab="",main=label)
+qqnorm(x)
+qqline(x)
+
+# Bimodal data
+new.window("bill")
+par(mfrow = c(2, 1))
+label <- "Bimodal Symmetric"
+x <- c(rnorm(100000,0,1),rnorm(100000,4,1)) 
+hist(x,col="#cccccc",breaks=100,axes=FALSE,xlab="",main=label)
+qqnorm(x)
+qqline(x)
