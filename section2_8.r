6b24bb3ed4f682a0aa13d218830bdf471e45f781
jarturomora <jarturomora@gmail.com>
2015-09-30 08:48:13 -0500

    A function to check normality was added to section2_8.r

--- section2_8.r
+++ section2_8.r
@@ -3,6 +3,20 @@
 # Import the new.window() function that you can find on my Github repo for this book at https://github.com/jarturomora/statistics-with-r-book
 source("new.window.r")
 
+# This function calculates the p-value of a dataset.
+# @param data : De data whose normality wants to be checked.
+# @return : TRUE if the data is normal, FALSE if it is not.
+is.normal <- function(data){
+	# Checkin normality with Shapiro Test.
+	pValue <- shapiro.test(data)
+	# The result of the shapiro.test function is a list, so we can access its elements to make fancy validation for normality.
+	result <- TRUE
+	if(pValue$p.value < 0.5) {
+		result <- FALSE
+	}
+	return(result)
+}
+
 # Setting up plot area
 par(mfrow = c(2,1))
 
@@ -17,6 +31,9 @@ hist(test.scores$score, breaks = 10, xlim = c(0,100), ylim = c(0,25), col = "blu
 # Now we create the QQ Plot
 qqnorm(test.scores$score)
 qqline(test.scores$score)
+
+# Checkin normality with Shapiro Test.
+if (is.normal(test.scores$score)) print ("The exam scores are normally distributed") else print("The exam scores are not normally distributed.")
 
 # Open a new window to plot normal data example
 new.window("bill")
