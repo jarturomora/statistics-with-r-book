6c841974421b46379e80391e2df3a37224d51523
jarturomora <jarturomora@gmail.com>
2015-10-01 08:49:15 -0500

    Code for Sectio 2.9 updated adding a colored scatterplot (still under coding)

--- section2_9.r
+++ section2_9.r
@@ -1,5 +1,8 @@
 # Code for Section 2.9 Scatterplots, Covariance, and Correlation
 
+# My windows opening function
+source("new.window.r")
+
 # Setting plot area
 par(mfrow = c(2, 1))
 
@@ -21,8 +24,50 @@ xdiff.x.ydiff <- xi.minus.xbar * yi.minus.ybar
 plot(xi, yi) # This is the scatteredplot from xi and yi
 # Printing all the calculations for the covariance
 calc.df <- cbind(i, xi, xbar, xi.minus.xbar, yi, ybar, yi.minus.ybar, xdiff.x.ydiff)
+print("-----------------------------------------------------------")
+print("Data for Covariance and Correlation Coefficient Calculation")
+print("-----------------------------------------------------------")
 print(calc.df)
-# Finally we calculate the covariance
+# Calculate the covariance
+print("Covariance Calculation")
+print("**********************")
 my.covariance <- sum(xdiff.x.ydiff)/9
 print(paste("My manually calculated covariance is: ", my.covariance, sep = ""))
 print(paste("The covariance calculated by R using the cov() function is: ", cov(xi, yi), sep = ""))
+# Calculate the correlation coefficient
+print("Correlation Coefficient Calculation")
+print("***********************************")
+print(paste("My manually correlation coefficient calculation: ", my.covariance/(sd(xi) * sd(yi)), sep = ""))
+print(paste("The correlation coefficient calculated by R using the cor() function: ", cor(xi, yi), sep = ""))
+
+# Now we will work with the data from the kshd-2013.txt file adding a new column for the seasson of the year based on the equinoxes and solstices.
+seasson <- rep(NA, 365)
+seasson[1:79] <- "winter"
+seasson[80:171] <- "spring"
+seasson[172:263] <- "summer"
+seasson[264:354] <- "fall"
+seasson[355:365] <- "winter"
+# Using the cbind() function "Column Bind" we attached the seasson vector to the weather data
+shd.wx <- cbind(shd.wx, seasson)
+print("---------------------------------------------------------")
+print("Weather Data from Nicole's Howtown With the Seasson Added")
+print("---------------------------------------------------------")
+print(head(shd.wx, 10))
+# Creating a new and nicer scatteredplot for the weather data with a different symbol for each seasson using the pch (Plot CHaracter) parameter in the plot() function.
+new.window("bill")
+plot(
+	shd.wx$TEMP, shd.wx$DEWP,
+	pch = as.integer(shd.wx$seasson),
+	col = as.integer(shd.wx$seasson),
+	xlab = "Average Daily Temperature in 2013",
+	ylab = "Dewpoint",
+	main = "Dewpoint Vs. Temperature (SHD 2013)"
+)
+# We add a nice legend to the plot
+legend(
+	"bottomright",
+	inset = c(0.02, 0.02),
+	legend = c("fall", "spring", "summer", "winter"),
+	pch = c(1:4),
+	col = palette()[1:4]
+)
