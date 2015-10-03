eff180acd0edd79a4266f603002340a66f35825a
jarturomora <jarturomora@gmail.com>
2015-10-03 08:57:14 -0500

    Code extended for Section 2.9 to improved the scatter plot for the wather conditions on Nicoles's hometown dataset

--- section2_9.r
+++ section2_9.r
@@ -40,34 +40,71 @@ print("***********************************")
 print(paste("My manually correlation coefficient calculation: ", my.covariance/(sd(xi) * sd(yi)), sep = ""))
 print(paste("The correlation coefficient calculated by R using the cor() function: ", cor(xi, yi), sep = ""))
 
-# Now we will work with the data from the kshd-2013.txt file adding a new column for the seasson of the year based on the equinoxes and solstices.
-seasson <- rep(NA, 365)
-seasson[1:79] <- "winter"
-seasson[80:171] <- "spring"
-seasson[172:263] <- "summer"
-seasson[264:354] <- "fall"
-seasson[355:365] <- "winter"
-# Using the cbind() function "Column Bind" we attached the seasson vector to the weather data
-shd.wx <- cbind(shd.wx, seasson)
-print("---------------------------------------------------------")
-print("Weather Data from Nicole's Howtown With the Seasson Added")
-print("---------------------------------------------------------")
+# Now we will work with the data from the kshd-2013.txt file adding a new column for the season of the year based on the equinoxes and solstices.
+season <- rep(NA, 365)
+season[1:79] <- "winter"
+season[80:171] <- "spring"
+season[172:263] <- "summer"
+season[264:354] <- "fall"
+season[355:365] <- "winter"
+# Using the cbind() function "Column Bind" we attached the season vector to the weather data
+shd.wx <- cbind(shd.wx, season)
+print("--------------------------------------------------------")
+print("Weather Data from Nicole's Howtown With the Season Added")
+print("--------------------------------------------------------")
 print(head(shd.wx, 10))
-# Creating a new and nicer scatteredplot for the weather data with a different symbol for each seasson using the pch (Plot CHaracter) parameter in the plot() function.
+# Creating a new and nicer scatteredplot for the weather data with a different symbol for each season using the pch (Plot CHaracter) parameter in the plot() function.
 new.window("bill")
 plot(
 	shd.wx$TEMP, shd.wx$DEWP,
-	pch = as.integer(shd.wx$seasson),
-	col = as.integer(shd.wx$seasson),
+	pch = as.integer(shd.wx$season),
+	col = as.integer(shd.wx$season),
 	xlab = "Average Daily Temperature in 2013",
 	ylab = "Dewpoint",
 	main = "Dewpoint Vs. Temperature (SHD 2013)"
 )
-# We add a nice legend to the plot
+# We add a nice legend to the plot but at this time we don't have an accurate assignation of colors.
 legend(
 	"bottomright",
+	title = "Non sure colors :-/",
 	inset = c(0.02, 0.02),
 	legend = c("fall", "spring", "summer", "winter"),
 	pch = c(1:4),
 	col = palette()[1:4]
 )
+# Now we will definne accurate colors for each season of the year.
+season.color <- rep(NA, 365)
+season.color[1:79] <- "gray"
+season.color[80:171] <- "red"
+season.color[172:263] <- "orange"
+season.color[264:354] <- "orange"
+season.color[355:365] <- "gray"
+# Now we add the colors column to our dataset
+shd.wx <- cbind(shd.wx, season.color)
+# Now we create a new plot with accurate labeling, legend and colors
+new.window("bill")
+plot(
+	shd.wx$TEMP, shd.wx$DEWP,
+	col = as.character(shd.wx$season.color),
+	pch = 15,
+	xlab = "Average Daily Temperature",
+	ylab = "Dewpoint",
+	main = "Dewpoint Vs Temperature (SHD 2013)"
+)
+legend(
+	"bottomright",
+	title = "Rigth Colors :-)",
+	inset = c(0.02, 0.02),
+	legend = c("fall", "spring", "summer", "winter"),
+	pch = 15,
+	col = c("orange", "green", "red", "gray")
+)
+# Catalog of the different plot points that can be used
+new.window("bill")
+plot.new()
+par(usr = c(-1, 21, 0, 1))
+for (n in 0:20) {
+	points(n, 0.5, pch = n, cex = 2)
+	text(n, 0.45, n)
+}
+box()
